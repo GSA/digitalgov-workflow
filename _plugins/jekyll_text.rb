8321db55c65cfe46d71dc9a5b4beb16f4042eecb
Jeremy Zilar <jeremyzilar@gmail.com>
2019-04-09 15:10:18 -0400

    adding in the lead text plugin

---
+++ _plugins/jekyll_text.rb
@@ -0,0 +1,16 @@
+module Jekyll
+  class RenderLeadTextBlock < Liquid::Block
+
+    def render(context)
+      text = super
+      site = context.registers[:site]
+      converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+      output = converter.convert(super(context))
+      "<div class=\"font-sans-xl text-light\">#{converter.convert(output)}</div>"
+
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('lead', Jekyll::RenderLeadTextBlock)
