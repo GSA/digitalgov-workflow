e20d7420b237588ee16bbe46205950710004a9ab
Jeremy Zilar <jeremyzilar@gmail.com>
2019-05-01 10:28:35 -0400

    re-working gulp setup in advance of a rewrite around blocks

---
+++ assets/js/src/blocks.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function ($) {
+
+  var entityPattern = /[&<>"'’`)(=+*@$%\/]/g;
+  function slugify(input) {
+    var output = input.split(" ").splice(0,6).join(" ");
+    output = output.replace(/[^a-zA-Z0-9\s]/g, "");
+    output = output.toLowerCase();
+    output = output.replace(/\s\s+/g, " ");
+    output = output.trim();
+    output = output.replace(/\s/g, "-");
+    return output;
+  }
+  function encodeEntities(input) {
+    var entityMap = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#39;',
+      "’": '&#39;',
+      '/': '&#x2F;',
+      '`': '&#x60;',
+      '=': '&#x3D;',
+      '*': '&#42;',
+      '$': '&#36;',
+      '%': '&#37;',
+      '(': '&#40;',
+      ')': '&#41;',
+      '+': '&#43;',
+      '@': '&#64;',
+      '-': '&#8208;',
+      '–': '&#8211;',
+      '—': '&#8212;'
+    };
+    return input.replace(entityPattern, function (s) {
+      return entityMap[s];
+    });
+  }
+
+  console.log('base');
+
+});
