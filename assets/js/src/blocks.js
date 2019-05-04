c474a6e713a59b90779d3851275e5213a2814ee6
Jeremy Zilar <jeremyzilar@gmail.com>
2019-05-04 11:29:04 -0400

    adding venue logic to events

--- assets/js/src/blocks.js
+++ assets/js/src/blocks.js
@@ -9,9 +9,10 @@ jQuery(document).ready(function ($) {
   var yearmoday = `${date.getFullYear()}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + (date.getDate()+1)).slice(-2)}`;
 
   // Get current time — not being used at the moment
-  var time = `${date.getHours()}:${(date.getMinutes()<10?'0':'') + date.getMinutes()}`;
+  var time = `${date.getHours()}:${(date.getMinutes()<10?'0':'') + '00'}`;
+  var time_end = `${date.getHours()+1}:${(date.getMinutes()<10?'0':'') + '00'}`;
   // Set time to 9am ET — our daily pub time
-  var time = '09:00';
+  // var time = '09:00';
 
   // returns the year and month for use in the filepath on GitHub
   // Returns: 2017/09
@@ -25,7 +26,12 @@ jQuery(document).ready(function ($) {
     return yearmo;
   }
 
-  $("#block-date input").val(yearmoday);
+  // Insert the time into the time fields
+  $("#block-date input, #block-date-end input").val(yearmoday);
+  $("#block-time input").val(time);
+  $("#block-time-end input").val(time_end);
+
+  $('#block-event_organizer input').val('Digital.gov');
 
 
   update_matter();
@@ -41,8 +47,8 @@ jQuery(document).ready(function ($) {
     var branch = "demo";
     post_matter += "---";
     post_matter += page_url_comment;
+    post_matter += "\n# Learn how to edit our pages at https://workflow.digital.gov\n";
     $('*[data-block]').each(function(){
-      var node = $(this).prop('nodeName');
   		var id = $(this).data('block'); // gets the id
   		var data_type = $(this).data('block-data_type'); // gets the data_type
   		var comment = $(this).data('block-comment') !== "" ? '\n# ' + $(this).data('block-comment') + '\n' : ""; // gets the comment
@@ -61,7 +67,7 @@ jQuery(document).ready(function ($) {
       }
 
   	});
-    post_matter += "\n\nLearn more about how to edit Digital.gov at https://workflow.digital.gov\n";
+    post_matter += "\n\n# ♥♥♥ Make it better ♥♥♥\n";
     post_matter += "---";
 
     $("#post-matter").html(post_matter);
@@ -89,7 +95,15 @@ jQuery(document).ready(function ($) {
     } else if (id == 'branch') {
       return 'skip';
     } else if (id == 'date') {
+      var time = $('#block-time input').val();
       return el.val() + ' ' + time;
+    } else if (id == 'date-end') {
+      var time = $('#block-time-end input').val();
+      return el.val() + ' ' + time;
+    } else if (id == 'time') {
+      return 'skip';
+    } else if (id == 'time-end') {
+      return 'skip';
     } else if (id == 'title') {
       return el.val();
     } else if (id == 'slug') {
@@ -99,16 +113,24 @@ jQuery(document).ready(function ($) {
       $(el).val(slug);
       return slug;
     } else if (id == 'filename') {
-      var slug = $('#block-slug input').val();
+      var title = $('#block-title input').val();
+      var slug = title.replace(new RegExp(small_words, "gi"), '');
+      var slug = slugify(slug);
       var filename = slug + '.md';
       $('#filename').text(filename);
       return 'skip';
     } else if (id == 'filename-dated') {
-      var slug = $('#block-slug input').val();
+      var title = $('#block-title input').val();
+      var slug = title.replace(new RegExp(small_words, "gi"), '');
+      var slug = slugify(slug);
       var date = $('#block-date input').val();
       var filename = date + '-' + slug + '.md';
       $('#filename').text(filename);
       return 'skip';
+    } else if (id == 'venue') {
+      return 'skip';
+    } else if (id == 'venue_name' || id == 'room' || id == 'address' || id == 'city' || id == 'state' || id == 'country' || id == 'zip' || id == 'map') {
+      return get_venue_info(id, el);
     } else {
       return el.val();
     }
@@ -136,7 +158,7 @@ jQuery(document).ready(function ($) {
 
   function get_page_url_comment(){
     var slug = $('#block-slug input').val();
-    var comment = "\n# View this page at https://digital.gov/" + file_yearmo() + slug + "\n";
+    var comment = "\n# View this page at https://digital.gov/" + file_yearmo() + slug;
     return comment;
   }
 
@@ -146,7 +168,7 @@ jQuery(document).ready(function ($) {
     var commit_msg = "New "+ content_type +": " + ($("#block-title input").val()).trim();
     var commit_desc = ($("#block-deck textarea").val()).trim();
 
-    if (content_type == 'blog_post' || content_type == 'events') {
+    if (content_type == 'posts' || content_type == 'events') {
       base_url += file_yearmo() + 'draft?filename=' + get_filename() + '&value=' + encodeURIComponent(post_matter) + '&message=' + encodeURIComponent(commit_msg) + '&description=' + encodeURIComponent(commit_desc) + '&target_branch=' + get_edit_branch();
     } else {
       base_url += 'draft?filename=' + get_filename() + '&value=' + encodeURIComponent(post_matter) + '&message=' + encodeURIComponent(commit_msg) + '&description=' + encodeURIComponent(commit_desc) + '&target_branch=' + get_edit_branch();
@@ -154,7 +176,11 @@ jQuery(document).ready(function ($) {
     return base_url;
   }
 
-
+  // Hide and show the Source and source_url fields
+  if ($('#card_display_dg').is(':checked') == true) {
+    $("#block-source, #block-source_url").addClass('display-none');
+    update_matter();
+  }
   $('#card_display input').click(function() {
     if($(this).is(':checked')){
       var val = $(this).val();
@@ -166,6 +192,25 @@ jQuery(document).ready(function ($) {
       update_matter();
     }
   });
+
+
+  // Venue information
+  $('#block-venue input').change(function() {
+    update_matter();
+  });
+  function get_venue_info(id, el){
+    // If Venue is not checked
+    if ($('#block-venue input').is(':checked') == false) {
+      // hide the venue fields
+      $('#block-'+id).addClass('display-none');
+      return 'skip';
+    } else {
+      // show the venue fields
+      $('#block-'+id).removeClass('display-none');
+      return el.val();
+    }
+  }
+
 
   var small_words = /\band |\bthe |\bare |\bis |\bof |\bto /gi;
 
